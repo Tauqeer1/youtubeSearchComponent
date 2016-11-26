bf62d1e5034f3b6904539431ef10323e6bb1bf34
tauqeer <tauqeer.shakir@yahoo.com>
2016-11-26 15:43:23 +0500

    Changes

--- src/app/search-box/search-box.component.ts
+++ src/app/search-box/search-box.component.ts
@@ -18,22 +18,24 @@ export class SearchBoxComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    // Convert the 'keyup' event into an observable stream
     Observable.fromEvent(this.el.nativeElement, 'keyup')
-      .map((e:any) => e.target.value)
-      .filter((text:string) => text.length > 1)
-      .debounceTime(250)
-      .do(() => this.loading.next(true))
-      .map((query:string) => this.youtube.search(query))
+      .map((e:any) => e.target.value) // extract the value of the input
+      .filter((text:string) => text.length > 1) // filter out if empty
+      .debounceTime(250) // only once every 250ms
+      .do(() => this.loading.next(true)) // enable loading
+      .map((query:string) => this.youtube.search(query)) // search, discarding old events if new input comes in
       .switch()
-      .subscribe((results:SearchResult[]) => {
+      // act on the return of the search
+      .subscribe((results:SearchResult[]) => { // on sucesss
           this.loading.next(false);
           this.results.next(results);
         },
-        (err:any) => {
+        (err:any) => { // on error
           console.log(err);
           this.loading.next(false);
         },
-        () => {
+        () => { // on completion
           this.loading.next(false);
         }
       );
